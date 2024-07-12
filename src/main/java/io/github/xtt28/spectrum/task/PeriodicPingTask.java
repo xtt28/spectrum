cd852a0435b0d20d8824db7c49c8d0716655e23f
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-07-11 20:40:54 -0400

    Actually ping servers in periodic ping task

--- src/main/java/io/github/xtt28/spectrum/task/PeriodicPingTask.java
+++ src/main/java/io/github/xtt28/spectrum/task/PeriodicPingTask.java
@@ -1,19 +1,30 @@
 package io.github.xtt28.spectrum.task;
 
+import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import br.com.azalim.mcserverping.MCPing;
+import br.com.azalim.mcserverping.MCPingOptions;
 import io.github.xtt28.spectrum.config.ConfigData;
 
 public class PeriodicPingTask implements Runnable {
 
     private ConfigData config;
+    private MCPingOptions options;
 
-    public PeriodicPingTask(ConfigData config) {
+    public PeriodicPingTask(ConfigData config, MCPingOptions options) {
         this.config = config;
+        this.options = options;
     }
 
     @Override
     public void run() {
-        Logger.getGlobal().info(String.format("The server %s would've been pinged now.", config.targetHostname()));
+        try {
+            var data = MCPing.getPing(options);
+            Logger.getGlobal().info(String.format("Player count on %s: %d/%d", config.targetHostname(), data.getPlayers().getOnline(), data.getPlayers().getMax()));
+        } catch (IOException ex) {
+            Logger.getGlobal().log(Level.WARNING, "Could not ping server.", ex);
+        }
     }
 }
--- src/main/java/io/github/xtt28/spectrum/task/PingScheduler.java
+++ src/main/java/io/github/xtt28/spectrum/task/PingScheduler.java
@@ -4,12 +4,19 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import br.com.azalim.mcserverping.MCPingOptions;
 import io.github.xtt28.spectrum.config.ConfigData;
 
 public class PingScheduler {
     
     public static void schedulePing(ConfigData config) {
-        var pinger = new PeriodicPingTask(config);
+        var options = MCPingOptions.builder()
+            .hostname(config.targetHostname())
+            .port(config.targetPort())
+            .timeout(config.targetTimeout())
+            .protocolVersion(config.targetProtocol())
+            .build();
+        var pinger = new PeriodicPingTask(config, options);
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
         executor.scheduleAtFixedRate(pinger, 0, config.pingDelay(), TimeUnit.MILLISECONDS);
     } 
